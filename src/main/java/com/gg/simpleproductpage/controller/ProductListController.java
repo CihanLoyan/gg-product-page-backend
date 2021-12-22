bc59c6433d44978baafab4adc23ba937cd303b14
CihanLoyan <cihanloyan@gmail.com>
2021-12-22 11:47:59 +0300

    RequestMapping Annotation added to ProductListController class

--- src/main/java/com/gg/simpleproductpage/controller/ProductListController.java
+++ src/main/java/com/gg/simpleproductpage/controller/ProductListController.java
@@ -3,20 +3,32 @@ package com.gg.simpleproductpage.controller;
 import com.gg.simpleproductpage.dto.ProductDto;
 import com.gg.simpleproductpage.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class ProductListController {
 
     @Autowired
     ProductService productService;
 
-    @GetMapping(value = "/get-product-list")
+    @GetMapping(value = "/products")
     public ResponseEntity<List<ProductDto>> getProductList(){
         return ResponseEntity.ok(productService.getProductList());
     }
+
+    @GetMapping(value = "/products/{id}")
+    public ResponseEntity<List<ProductDto>> getProduct(@PathVariable int id){
+        return ResponseEntity.ok(productService.getProduct(id));
+    }
+
+    /*@GetMapping("/get-product-list/get")
+    public ResponseEntity<List<ProductDto>> getProduct(@RequestParam int id){
+        return ResponseEntity.ok(productService.getProduct(id));
+    }*/
+
 }
